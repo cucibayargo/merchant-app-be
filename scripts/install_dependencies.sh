999dc638f82f7bcbdcd0d15b7c5eb6412bb9a3c3
saiful <muhammad.saiful.engineer@gmail.com>
2024-12-17 23:47:16 +0700

    ci(pipeline): fix code pipeline folder

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /var/www/html
+cd /home/ec2-user/cucibayargo
 npm install --production
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /var/www/html
+cd /home/ec2-user/cucibayargo
 pm2 stop all || true
 pm2 start dist/api.js --name "node-app"
