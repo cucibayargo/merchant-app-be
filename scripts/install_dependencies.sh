1e61caa14dfdb0e3fce57bb2a2f402590347ac5f
saiful <muhammad.saiful.engineer@gmail.com>
2024-12-19 00:46:17 +0700

    build(codedeploy): fix permission issues

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user/cucibayargo
-npm install --production
+pwd  # Check the current directory, to ensure you're in the correct location
+npm install --omit=dev  # Install production dependencies only
