378fcf564e077f3d1535c4cf753376a3ea04aef9
saiful <muhammad.saiful.engineer@gmail.com>
2024-12-19 02:16:25 +0700

    ci(codedeploy): reinstall pm2 as code runner

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 cd /home/ec2-user/cucibayargo
+
+# Check if pm2 is installed
+if ! command -v pm2 &> /dev/null; then
+    echo "pm2 could not be found, installing it..."
+    npm install -g pm2
+fi
+
 pm2 stop all || true
 pm2 start dist/api.js --name "node-app"
