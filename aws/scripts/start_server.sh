11c7dea912c6969e358d6900f132b2ad30e5b332
saiful <muhammad.saiful.engineer@gmail.com>
2024-12-19 23:55:16 +0700

    refactor(codebuild): grab all files in aws folder

--- aws/scripts/start_server.sh
+++ aws/scripts/start_server.sh
@@ -2,7 +2,7 @@
 cd /home/ec2-user/cucibayargo
 
 # Check if pm2 is installed
-if ! command -v pm2 &> /dev/null; then
+if ! command -v /usr/local/bin/pm2 &> /dev/null; then
     echo "pm2 could not be found, installing it..."
     sudo npm install -g pm2
 fi
