9f3b760c2e4f7a0056b769c569c2dce0e7ed7169
saiful <muhammad.saiful.engineer@gmail.com>
2025-03-01 15:44:00 +0700

    fix(aws): remove unused build scripts

--- aws/scripts/install_dependencies.sh
+++ aws/scripts/install_dependencies.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
 cd /home/ec2-user/cucibayargo
-ls
-pwd  # Check the current directory, to ensure you're in the correct location
-npm install --omit=dev  # Install production dependencies only
--- aws/scripts/start_server.sh
+++ aws/scripts/start_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/cucibayargo
+npm install --omit=dev  
 
 # Stop any existing Node.js process on port 3000 (if running)
 fuser -k 3000/tcp || true
