08442cf2127f871e9d33fc82b46ee917b11d3fa5
saiful <muhammad.saiful.engineer@gmail.com>
2026-04-28 22:29:54 +0700

    fix(customer): fix customer validation

--- src/modules/customer/types.ts
+++ src/modules/customer/types.ts
@@ -18,7 +18,7 @@ export const customerSchema = Joi.object({
     'string.email': 'Email tidak valid',
   }),
   address: Joi.string().allow(''), // Alamat tidak wajib diisi
-  phone_number: Joi.string().pattern(/^[+]?[0-9]{10,15}$/).messages({
+  phone_number: Joi.string().allow('').pattern(/^[+]?[0-9]{10,15}$/).messages({
     'string.pattern.base': 'Nomor telepon tidak valid',
   }),
   gender: Joi.string().valid('Laki-laki', 'Perempuan').required().messages({
